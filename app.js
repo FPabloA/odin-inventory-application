88f1fa37cdf7443ab3f6fc1db86286f957658fe6
FPabloA <pabloayala@protonmail.com>
2025-09-25 15:08:34 -0500

    added files for index page

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static(assetsPath));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use("/", router);
+app.use("/", indexRouter);
 
 app.listen(port, (error) => {
     if (error){

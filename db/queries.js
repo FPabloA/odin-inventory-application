63ec325dc12068fba9e26a8fdba685edf2cc870b
FPabloA <pabloayala@protonmail.com>
2025-09-30 13:03:41 -0500

    added additional functionality for adding, updating and deleting products to db

--- db/queries.js
+++ db/queries.js
@@ -20,6 +20,89 @@ async function filterById(id) {
     return rows[0];
 }
 
+async function addProductToDb(newProduct) {
+    const genreId = await getGenreIdByName(newProduct.genre);
+    if(!genreId) {
+        throw new Error("Invalid Category");
+    }
+
+    await pool.query(
+        "INSERT INTO inventory ( name, quantity, price, brand, description, genre_id, src, isDefault) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+        [
+            newProduct.name,
+            newProduct.quantity,
+            newProduct.price,
+            newProduct.artist,
+            newProduct.description,
+            genreId,
+            newProduct.src,
+            false,
+        ]
+    );
+}
+
+async function deleteProduct(id) {
+    await pool.query(
+        "DELETE FROM inventory WHERE id = $1 AND isDefault = false",
+        [id]
+    );
+}
+
+async function editProduct({id, name, quantity, price, genre, artist, src, description}) {
+    const updates = [];
+    const values = [];
+    let index = 1;
+
+    if (name) {
+        updates.push(`name =$${index++}`);
+        values.push(name);
+    }
+
+    if (quantity) {
+        updates.push(`quantity = $${index++}`);
+        values.push(quantity);
+    }
+
+    if (price) {
+        updates.push(`price = $${index++}`);
+        values.push(price);
+    }
+
+    if (genre) {
+        const genreId = await getGenreIdByName(genre);
+        if (genreId) {
+            updates.push(`genre_id = $${index++}`);
+            values.push(genreId);
+        }
+        else{
+            throw new Error("Invalid category");
+        }
+    }
+
+    if (artist) {
+        updates.push(`brand = $${index++}`);
+        values.push(artist);
+    }
+
+    if (src) {
+        updates.push(`src = $${index++}`);
+        values.push(src);
+    }
+
+    if (description) {
+        updates.push(`description = $${index++}`);
+        values.push(description);
+    }
+
+    if (updates.length > 0) {
+        const query = `UPDATE inventory SET ${updates.join(
+            ", "
+        )} WHERE id =$${index}`;
+        values.push(id);
+        await pool.query(query, values);
+    }
+}
+
 async function getGenres() {
     const { rows } = await pool.query(
         "SELECT * FROM genres ORDER BY genre ASC;"
@@ -40,4 +123,7 @@ module.exports = {
     getGenres,
     getGenreIdByName,
     filterById,
+    addProductToDb,
+    deleteProduct,
+    editProduct,
 };
